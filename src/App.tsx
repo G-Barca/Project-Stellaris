503b60089db84870ed4a189c791a95eba8f2d7fe
g-barca <oguilhermebarca@gmail.com>
2026-05-28 16:03:40 -0300

    feat: Ajustes iniciais do vite e tailwind

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-gray-900 min-h-screen flex items-center justify-center">
+      <h1 className="text-white text-4xl">teste</h1>
+    </div>
+  )
+}
+
+export default App
